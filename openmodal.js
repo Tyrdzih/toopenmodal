a6395032a42f501522a9de4b4ad7c09d3249435a
Tyrdzih <66749806+Tyrdzih@users.noreply.github.com>
2024-01-18 01:44:09 -0500

    Add files via upload

---
+++ openmodal.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  // https://getbootstrap.com/docs/5.0/components/modal/#via-javascript
+  let myModal = new bootstrap.Modal(
+    document.getElementById("myModal"),
+    {}
+  );
+  // https://getbootstrap.com/docs/5.0/components/modal/#show
+  myModal.show();
+};
