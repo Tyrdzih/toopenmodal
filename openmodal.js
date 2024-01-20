ef97f104fa837162368defbaf636d0f3de2c0181
Tyrdzih <66749806+Tyrdzih@users.noreply.github.com>
2024-01-20 17:05:32 -0500

    Update openmodal.js

--- openmodal.js
+++ openmodal.js
@@ -2,9 +2,8 @@ window.onload = function () {
   // https://getbootstrap.com/docs/5.0/components/modal/#via-javascript
   let myModal = new bootstrap.Modal(
     document.getElementById("myModal"),
-    {}
+    {backdrop: "static"}
   );
-  $('#myModal').modal({'data-bs-backdrop': 'static', 'data-bs-keyboard': false});
   // https://getbootstrap.com/docs/5.0/components/modal/#show
   myModal.show();
 };
